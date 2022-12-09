57125b4959c4a73fa95dd253a830fb83f50d540e
Rebecca Troy <20088744@mail.wit.ie>
2022-12-09 14:32:10 +0000

    Topic 11 - Salt and Hash

--- movies-api/seedData/index.js
+++ movies-api/seedData/index.js
@@ -8,14 +8,14 @@ dotenv.config();
 
 // deletes all user documents in collection and inserts test data
 async function loadUsers() {
-  console.log('load user Data');
-  try {
-    await userModel.deleteMany();
-    await userModel.collection.insertMany(users);
-    console.info(`${users.length} users were successfully stored.`);
-  } catch (err) {
-    console.error(`failed to Load user Data: ${err}`);
-  }
+    console.log('load user Data');
+    try {
+      await userModel.deleteMany();
+      await users.forEach(user => userModel.create(user));
+      console.info(`${users.length} users were successfully stored.`);
+    } catch (err) {
+      console.error(`failed to Load user Data: ${err}`);
+    }
 }
 
 // deletes all genre documents in collection and inserts test data
