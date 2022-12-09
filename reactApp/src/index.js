fb65118fd47ada5663d3ee9f249418215bfba423
Rebecca Troy <20088744@mail.wit.ie>
2022-12-09 18:06:40 +0000

    Topic 12 - Sign up

--- reactApp/src/index.js
+++ reactApp/src/index.js
@@ -7,6 +7,7 @@ import LoginPage from "./loginPage";
 import AuthProvider from "./authContext";
 import AuthHeader from "./authHeader";
 import ProtectedRoutes from "./protectedRoutes";
+import SignUpPage from "./signUpPage";
 
 const App = () => {
   return (
@@ -38,6 +39,7 @@ const App = () => {
           </Route>
 
           <Route path="*" element={ <Navigate to="/" /> } />
+          <Route path="/signup" element={ <SignUpPage /> } />
         </Routes>
       </AuthProvider>
     </BrowserRouter>
