8e255e78976aa9d435eaa6a04a8df0520a05134a
Rebecca Troy <20088744@mail.wit.ie>
2022-12-09 19:19:12 +0000

    Topic 12 - Movies context

--- reactApp/src/index.js
+++ reactApp/src/index.js
@@ -8,6 +8,7 @@ import AuthProvider from "./authContext";
 import AuthHeader from "./authHeader";
 import ProtectedRoutes from "./protectedRoutes";
 import SignUpPage from "./signUpPage";
+import MovieProvider from "./moviesContext";
 
 const App = () => {
   return (
@@ -28,19 +29,21 @@ const App = () => {
             <Link to="/profile">Profile</Link>
           </li>
         </ul>
-        <Routes>
-          <Route path="/public" element={ <PublicPage /> } />
-          <Route path="/" element={ <HomePage /> } />
-          <Route path="/login" element={ <LoginPage /> } />
+        <MovieProvider>
+          <Routes>
+            <Route path="/public" element={ <PublicPage /> } />
+            <Route path="/" element={ <HomePage /> } />
+            <Route path="/login" element={ <LoginPage /> } />
 
-          <Route element={<ProtectedRoutes />}>
-            <Route path="/movies" element={<Movies />} />
-            <Route path="/profile" element={<Profile />} />
-          </Route>
+            <Route element={<ProtectedRoutes />}>
+              <Route path="/movies" element={<Movies />} />
+              <Route path="/profile" element={<Profile />} />
+            </Route>
 
-          <Route path="*" element={ <Navigate to="/" /> } />
-          <Route path="/signup" element={ <SignUpPage /> } />
-        </Routes>
+            <Route path="*" element={ <Navigate to="/" /> } />
+            <Route path="/signup" element={ <SignUpPage /> } />
+          </Routes>
+        </MovieProvider>
       </AuthProvider>
     </BrowserRouter>
   );
