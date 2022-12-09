fb3f4f85839b9dd75de646042994aeabea350ea6
Rebecca Troy <20088744@mail.wit.ie>
2022-12-09 14:16:16 +0000

    Mongo lab Exercises done

--- movies-api/api/genres/index.js
+++ movies-api/api/genres/index.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import {genres} from './genresData';
+import Genre from './genreModel';
 
 const router = express.Router(); 
 
-router.get('/', (req, res) => {
-    res.json(genres);
-    console.log(genres);
+// Get all genres
+router.get('/', async (req, res) => {
+    const genres = await Genre.find();
+    res.status(200).json(genres);
 });
 
 export default router;
